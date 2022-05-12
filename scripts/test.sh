22a4efd8f19ca51284c00d9078b9168b03355fbc
Frankie Gallina-Jones <frankieg@vmware.com>
2022-05-12 18:45:43 +0000

    initial scripts outline

---
+++ scripts/test.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+readonly PROG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly STACK_DIR="$(cd "${PROG_DIR}/.." && pwd)"
+
+# shellcheck source=SCRIPTDIR/.util/tools.sh
+source "${PROG_DIR}/.util/tools.sh"
+
+# shellcheck source=SCRIPTDIR/.util/print.sh
+source "${PROG_DIR}/.util/print.sh"
+
+function main() {
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        util::print::error "unknown argument \"${1}\""
+    esac
+  done
+
+  mkdir -p "${STACK_DIR}/build"
+
+  tools::install
+}
+
+function usage() {
+  cat <<-USAGE
+create.sh [OPTIONS]
+
+Creates the stack using the descriptor, build and run Dockerfiles in
+the repository.
+
+OPTIONS
+  --help  -h  prints the command usage
+USAGE
+}
+
+function tools::install() {
+  util::tools::jam::install \
+    --directory "${STACK_DIR}/.bin"
+
+  util::tools::skopeo::check
+}
+
+main "${@:-}"
